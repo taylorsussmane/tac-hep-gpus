20d1d00c05d75bfda37430b548fd120efc460e2e
Taylor Sussmane <tsussmane1@gmail.com>
2025-12-05 16:34:09 -0600

    Implemented non-default streams and shared mem for stencil. Shared mem for matrix mult not currently working.

--- Project/optimizedCudaMatrixOps.cu
+++ Project/optimizedCudaMatrixOps.cu
@@ -9,9 +9,41 @@ const int DSIZE = N+2*RADIUS;
 const int A_val = 1;
 const int B_val = 2;
 
-//--------------MATRIX MULTIPLICATION--------------
+//--------------MATRIX MULTIPLICATION W/ SHARED MEM--------------
+// reference: https://github.com/kberkay/Cuda-Matrix-Multiplication/blob/master/matrix_Multiplication.cu
 __global__ void matrix_mult(const int *A, const int *B, int *C, int size) {
+	// put smaller tiles of matrix into shared mem
+	__shared__ int tileA[BLOCK_SIZE][BLOCK_SIZE];
+	__shared__ int tileB[BLOCK_SIZE][BLOCK_SIZE];
 
+	int idx = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+	int idy = blockIdx.y * BLOCK_SIZE + threadIdx.y;
+	
+	int temp = 0;
+
+	// loop through tiles grid
+	for (int ntile = 0; ntile < (BLOCK_SIZE+DSIZE-1); ntile++){
+		// Make coalesced mem access
+		tileA[threadIdx.y][threadIdx.x] = A[idy*DSIZE+(ntile*BLOCK_SIZE+threadIdx.x)];
+		tileB[threadIdx.y][threadIdx.x] = B[(ntile*BLOCK_SIZE+threadIdx.y)*DSIZE+idx];
+
+		__syncthreads();
+
+		// do the matrix multiplication for one tile
+		for (int i = 0; i < BLOCK_SIZE; i++){	
+			temp+= tileA[threadIdx.y][i]*tileB[i][threadIdx.x];
+		}
+		__syncthreads();
+	}
+	// store result
+	C[idy*DSIZE+idx] = temp;
+}
+
+
+
+
+
+/*
     // create thread x index
     // create thread y index
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -24,32 +56,46 @@ __global__ void matrix_mult(const int *A, const int *B, int *C, int size) {
         }
         C[idy*size+idx] = tempSum;
     }
-}
+}*/
 
 //-----------------2D STENCIL-----------------
 __global__ void stencil_2d(int *in, int *out) {
+	__shared__ int temp[BLOCK_SIZE + 2 * RADIUS][BLOCK_SIZE + 2 * RADIUS];
+	int gindex_x = threadIdx.x + blockIdx.x * blockDim.x;
+	int lindex_x = threadIdx.x + RADIUS;
+	int gindex_y = threadIdx.y + blockIdx.y * blockDim.y;
+	int lindex_y = threadIdx.y + RADIUS;
 
-    int gindex_x = threadIdx.x + blockIdx.x * blockDim.x;
-    int gindex_y = threadIdx.y + blockIdx.y * blockDim.y;
+	// Read input elements into shared memory
+	int size = N + 2 * RADIUS;
+	temp[lindex_x][lindex_y] = in[gindex_y+size*gindex_x];
 
-    // Read input elements into shared memory
-    int size = N + 2 * RADIUS;
+	if (threadIdx.x < RADIUS) {
+		temp[lindex_x-RADIUS][lindex_y] = in[size*(gindex_x - RADIUS)+gindex_y];
+		temp[lindex_x+BLOCK_SIZE][lindex_y] = in[size*(gindex_x+BLOCK_SIZE)+gindex_y];
+	}
 
-    __syncthreads();
+	if (threadIdx.y < RADIUS ) {
+		temp[lindex_x][lindex_y-RADIUS] = in[gindex_x*size+(gindex_y-RADIUS)];
+		temp[lindex_x][lindex_y+BLOCK_SIZE] = in[gindex_x*size+(gindex_y+BLOCK_SIZE)];
+	}
 
-    // Apply the stencil
-    int result = 0;
-    for (int offset = -RADIUS; offset <= RADIUS; offset++){
-        result += in[gindex_y+(gindex_x+offset)*size];
-        result += in[gindex_y+offset+gindex_x*size];
-    }
+	__syncthreads();
 
-    // In the loop, this index is double counted, so this fixes that.
-    result -= in[gindex_y+gindex_x*size];
+	// Apply the stencil
+	int result = 0;
+	for (int offset = -RADIUS; offset <= RADIUS; offset++){
+		result += temp[lindex_x+offset][lindex_y];
+		result += temp[lindex_x][lindex_y+offset];
+	}
 
-    // Store the result
-    out[gindex_y+size*gindex_x] = result;
+	// Avoid double counting
+	result -= temp[lindex_x][lindex_y];
+
+	// Store the result
+	out[gindex_y+size*gindex_x] = result;
 }
+
 
 //-----------------2D STENCIL VALUE CHECK-----------------
 int stencil_error_check(const int *in, const int *out, const int val){
@@ -143,12 +189,21 @@ int main(void){
 	
 	int size = (N+2*RADIUS)*(N+2*RADIUS)*sizeof(int);
 
+	// create streams
+	cudaStream_t stream1, stream2;
+	cudaStreamCreate(&stream1);
+	cudaStreamCreate(&stream2);
+	
 	// allocate space for device copies with managed memory
 	cudaMallocManaged((void **)&h_a, size);
+	cudaStreamAttachMemAsync(stream1, h_a, size);
 	cudaMallocManaged((void **)&h_b, size);
+	cudaStreamAttachMemAsync(stream2, h_b, size);
 	cudaMallocManaged((void **)&h_c, size);
 	cudaMallocManaged((void **)&h_stenciled_a, size);
+	cudaStreamAttachMemAsync(stream1, h_stenciled_a, size, cudaMemAttachGlobal);
 	cudaMallocManaged((void **)&h_stenciled_b, size);
+	cudaStreamAttachMemAsync(stream2, h_stenciled_b, size, cudaMemAttachGlobal);
 	cudaCheckErrors("Error when allocating memory.");
 	
 	// initialize values
@@ -162,21 +217,25 @@ int main(void){
 	int gridSize = (N + BLOCK_SIZE-1)/BLOCK_SIZE;
 	dim3 grid(gridSize, gridSize);
 	dim3 block(BLOCK_SIZE, BLOCK_SIZE);
-	
-	// launch stencil kernels on a and b and then launch matrix multiplication 
-	stencil_2d<<<grid,block>>>(h_a + RADIUS*(N + 2*RADIUS) + RADIUS , h_stenciled_a + RADIUS*(N + 2*RADIUS) + RADIUS);
-	stencil_2d<<<grid,block>>>(h_b + RADIUS*(N + 2*RADIUS) + RADIUS , h_stenciled_b + RADIUS*(N + 2*RADIUS) + RADIUS);
-	cudaCheckErrors("Error when running stencil kernels.");
 
-	int m_gridSize = (DSIZE+BLOCK_SIZE-1)/BLOCK_SIZE;
-	dim3 m_grid(m_gridSize, m_gridSize);
-	dim3 m_block(BLOCK_SIZE, BLOCK_SIZE);
-	matrix_mult<<<m_grid,m_block>>>(h_stenciled_a, h_stenciled_b, h_c, DSIZE);
-	cudaCheckErrors("Error when running multiplication kernel.");
+	// launch stencil kernels in streams 1 and 2 
+	stencil_2d<<<grid,block,0,stream1>>>(h_a + RADIUS*(N + 2*RADIUS) + RADIUS , h_stenciled_a + RADIUS*(N + 2*RADIUS) + RADIUS);
+	stencil_2d<<<grid,block,0,stream2>>>(h_b + RADIUS*(N + 2*RADIUS) + RADIUS , h_stenciled_b + RADIUS*(N + 2*RADIUS) + RADIUS);
+	cudaCheckErrors("Error when running stencil kernels.");
 	
 	// synchronize before accessing data on host
 	cudaDeviceSynchronize();
 
+	// synchronize streams
+	cudaStreamSynchronize(stream1);
+	cudaStreamSynchronize(stream2);
+
+	// launch matrix mult
+	int m_gridSize = DSIZE/BLOCK_SIZE;
+	dim3 m_grid(m_gridSize, m_gridSize);
+	dim3 m_block(BLOCK_SIZE, BLOCK_SIZE);
+	matrix_mult<<<m_grid,m_block>>>(h_stenciled_a, h_stenciled_b, h_c, DSIZE);
+	cudaCheckErrors("Error when running multiplication kernel.");
 	
 	// check results
 	stencil_error_check(h_a, h_stenciled_a, A_val);
@@ -189,5 +248,9 @@ int main(void){
 	cudaFree(h_c);
 	cudaFree(h_stenciled_a);
 	cudaFree(h_stenciled_b);
+
+	// destroy streams
+	cudaStreamDestroy(stream1);
+	cudaStreamDestroy(stream2);
 
 }
